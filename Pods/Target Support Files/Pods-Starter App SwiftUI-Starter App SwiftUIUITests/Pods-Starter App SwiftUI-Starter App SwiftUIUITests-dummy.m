384602488c81e8e968a0f03843c5d263ebefe7b1
fadlurahmanf <tffajari@gmail.com>
2023-01-04 19:30:56 +0700

    initial message

---
+++ Pods/Target Support Files/Pods-Starter App SwiftUI-Starter App SwiftUIUITests/Pods-Starter App SwiftUI-Starter App SwiftUIUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Starter_App_SwiftUI_Starter_App_SwiftUIUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Starter_App_SwiftUI_Starter_App_SwiftUIUITests
+@end
